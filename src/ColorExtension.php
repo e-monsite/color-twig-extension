6a587a88d02a4d83ba387c2732eba6bc210a1c75
Jonathan Plantey <jonathan@e-monsite.com>
2019-11-13 15:31:34 +0000

    Initial commit

---
+++ src/ColorExtension.php
@@ -0,0 +1,104 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Emonsite\ColorTwigExtension;
+
+use MikeAlmond\Color\Color;
+use MikeAlmond\Color\CssGenerator;
+use MikeAlmond\Color\Exceptions\InvalidColorException;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+/**
+ * Some filter to manipulate colors in twig
+ */
+class ColorExtension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('darken', [$this, 'darken']),
+            new TwigFilter('lighten', [$this, 'lighten']),
+            new TwigFilter('alpha', [$this, 'alpha']),
+        ];
+    }
+
+    public function darken(?string $colorAsString, int $percent): string
+    {
+        try {
+            /** @var Color $color */
+            [$color, $alpha] = $this->parseColor($colorAsString);
+        } catch (InvalidColorException $e) {
+            return (string) $colorAsString;
+        }
+
+        return CssGenerator::rgba($color->darken($percent), $alpha);
+    }
+
+    public function lighten(?string $colorAsString, int $percent): string
+    {
+        try {
+            /** @var Color $color */
+            [$color, $alpha] = $this->parseColor($colorAsString);
+        } catch (InvalidColorException $e) {
+            return (string) $colorAsString;
+        }
+
+        return CssGenerator::rgba($color->lighten($percent), $alpha);
+    }
+
+    /**
+     * Set the alpha (transparence) of a color
+     */
+    public function alpha(?string $colorAsString, float $newAlpha): string
+    {
+        try {
+            /** @var Color $color */
+            [$color,] = $this->parseColor($colorAsString);
+        } catch (InvalidColorException $e) {
+            return $colorAsString;
+        }
+
+        return CssGenerator::rgba($color, $newAlpha);
+    }
+
+
+    /**
+     * @return array [Color $color, mixed $alpha]
+     * @throws InvalidColorException if unable to parse to a proper color
+     */
+    private function parseColor(string $colorAsString): array
+    {
+        $colorAsString = mb_strtolower($colorAsString);
+
+        // parse hexa
+        if (mb_strpos($colorAsString, '#') === 0) {
+            return [Color::fromHex($colorAsString), '1'];
+        }
+
+        // parse rgb(a)
+        if (mb_strpos($colorAsString, 'rgb') === 0) {
+            $colorAsString = str_replace(['rgba', 'rgb'], '', $colorAsString);
+            $colorAsString = trim($colorAsString, '()');
+            $parts = explode(',', $colorAsString);
+
+            switch (count($parts)) {
+                case 3:
+                    [$r, $g, $b] = $parts;
+                    break;
+                case 4:
+                    [$r, $g, $b, $alpha] = $parts;
+                    break;
+                default:
+                    throw new InvalidColorException('error parsing rgba color');
+            }
+
+
+            return [Color::fromRgb((int) $r, (int) $g, (int) $b), $alpha ?? 1];
+        }
+
+        // fallback in "name" (ex : green)
+        return [Color::fromCssColor($colorAsString), 1];
+    }
+}
---
+++ tests/ColorExtensionTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Emonsite\ColorTwigExtension\Tests;
+
+use Emonsite\ColorTwigExtension\ColorExtension;
+use PHPUnit\Framework\TestCase;
+
+class ColorExtensionTest extends TestCase
+{
+    /** @var ColorExtension */
+    private static $extension;
+
+    public static function setUpBeforeClass(): void
+    {
+        static::$extension = new ColorExtension();
+    }
+
+    /**
+     * @dataProvider darkenColorProvider
+     */
+    public function testDarken(string $color, int $percent, string $expectedResult)
+    {
+        static::assertSame($expectedResult, static::$extension->darken($color, $percent));
+    }
+
+    public function darkenColorProvider()
+    {
+        return [
+            ['rgb(138, 7, 7)', 20, 'rgba(110, 5, 5, 1)'],
+            ['rgb(138, 7, 7, 0.8)', 20, 'rgba(110, 5, 5, 0.8)'],
+            ['#8a0707', 20, 'rgba(110, 5, 5, 1)'],
+        ];
+    }
+
+    public function testAlpha()
+    {
+        static::assertSame('rgba(110, 5, 5, 0.5)', static::$extension->alpha('rgba(110, 5, 5, 1)', 0.5));
+    }
+}
